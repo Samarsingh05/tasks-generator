8215632702af880b17eebde49fc615db9cae6338
Samar Singh <samarsingh@MB.local>
2026-02-12 20:43:53 +0530

    Deploy to GitHub Pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/tasks-generator/',
+  build: {
+    outDir: 'docs'
+  },
+  server: {
+    port: 5173
+  }
 })
